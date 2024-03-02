3078940b5be6c85a752e1c8b7ce29f539cb8a605
Christian Briones <christian.briones.oliveros@outlook.com>
2024-03-02 13:59:23 -0500

    Small bug fix GetHtmlTag

--- src/Feature/SEO/IASearchEngineOptimization/Gemini.BusinessLogic/HtmlTagHelper.cs
+++ src/Feature/SEO/IASearchEngineOptimization/Gemini.BusinessLogic/HtmlTagHelper.cs
@@ -12,14 +12,14 @@
                 case RequestType.Title:
                     var titleTag = input[input.IndexOf("<title>")..];
                     int titleEndIndex = titleTag.IndexOf("</title>");
-                    return titleTag[..(titleEndIndex + "</title>".Length)];
+                    return titleEndIndex == -1 ? "Not generated, try again!" : titleTag[..(titleEndIndex + "</title>".Length)];
                 case RequestType.MetaDescription:
                 case RequestType.Keywords:
                     var metaTag = input[input.IndexOf("<meta")..];
                     int metaEndIndex = metaTag.IndexOf("/>");
                     if (metaEndIndex == -1)
                         metaEndIndex = metaTag.IndexOf(">");
-                    return metaTag[..(metaEndIndex)] + ">";
+                    return metaEndIndex == -1 ? "Not generated, try again!" : metaTag[..(metaEndIndex)] + ">";
                 default:
                     return string.Empty;
             }
